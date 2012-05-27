e34ec5a78a86c3fac71529b82d7d298bbfaddb87
tomoya ishida <tomoyapenguin@gmail.com>
2012-05-27 19:05:28 +0900

    chartable change

--- charinfo/CharInfo.java
+++ charinfo/CharInfo.java
@@ -3,7 +3,6 @@ import java.awt.*;
 import java.awt.image.*;
 import javax.imageio.*;
 import java.awt.geom.*;
-//not optimized. (see charindex.png)
 class CharInfo{
 	static char chars[]={
 32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126};
@@ -35,7 +34,7 @@ class CharInfo{
 			for(int k=0;k<2;k++){
 				int sum=0;
 				for(int x=0;x<CW;x++)for(int y=0;y<CH/2;y++)sum+=img.getRGB(x,y+CH/2*k)&0xff;
-				infos[i][k]=sum;
+				infos[i][k]=sum*2.0/CH/CW;
 			}
 			//ImageIO.write(img,"png",new File("chars/"+(int)chars[i]+".png"));
 		}
@@ -43,13 +42,41 @@ class CharInfo{
 	}
 	static void makeIndex(String fname)throws Exception{
 		BufferedImage img=new BufferedImage(256,256,BufferedImage.TYPE_INT_RGB);
-		
-		double min,max;min=max=infos[0][0];
-		for(int i=0;i<infos.length;i++)for(int k=0;k<2;k++){
-			if(infos[i][k]<min)min=infos[i][k];
-			if(infos[i][k]>max)max=infos[i][k];
+		double difmap[][]=new double[256][256];
+		for(int i=0;i<256;i++)for(int j=0;j<256;j++){
+			double difmin=Double.MAX_VALUE;
+			int im=0;
+			for(int ii=0;ii<infos.length;ii++){
+				double[]info=infos[ii];
+				double dif=(i-info[0])*(i-info[0])+(j-info[1])*(j-info[1]);
+				if(dif<difmin){difmin=dif;im=ii;}
+			}
+			difmap[i][j]=difmin;
+			img.setRGB(i,j,(int)(0x7fffffff*Math.sin(im+0xffffff*Math.sin(im+1)+2)));
 		}
-		for(int i=0;i<infos.length;i++)for(int k=0;k<2;k++)infos[i][k]=255*(infos[i][k]-min)/(max-min);
+		ImageIO.write(img,"png",new File("charindex_before.png"));
+		double minmaxResult[][]=new double[256][256];
+		double allrange=0;
+		for(int i=0;i<256;i++)for(int j=0;j<256;j++)allrange+=difmap[i][j];
+		
+		for(int min=0;min<256;min++){
+			double sumall=allrange;
+			for(int max=255;max>=min;max--){
+				minmaxResult[min][max]=sumall;
+				sumall-=difmap[max][max];
+				for(int i=min;i<max;i++)sumall-=difmap[max][i]+difmap[i][max];
+			}
+			allrange-=difmap[min][min];
+			for(int i=min+1;i<256;i++)allrange-=difmap[min][i]+difmap[i][min];
+		}
+		int optMin=0,optMax=255;double optDif=Double.MAX_VALUE;
+		for(int min=0;min<256-1;min++)for(int max=min+1;max<256;max++){
+			if(max!=255)continue;//force using ' ' for color #ffffff
+			double dif=Math.sqrt(minmaxResult[min][max])/(max-min+1)/(max-min+1);
+			if(dif<optDif){optDif=dif;optMin=min;optMax=max;}
+		}
+		System.out.println(optMin+" "+optMax+" "+optDif);
+		for(int i=0;i<infos.length;i++)for(int k=0;k<2;k++)infos[i][k]=255*(infos[i][k]-optMin)/(optMax-optMin);
 		
 		BufferedOutputStream out=new BufferedOutputStream(new FileOutputStream(new File(fname)));
 		for(int i=0;i<256;i++)for(int j=0;j<256;j++){
@@ -64,7 +91,7 @@ class CharInfo{
 			out.write(chars[im]);
 		}
 		out.close();
-		ImageIO.write(img,"png",new File("charindex.png"));
+		ImageIO.write(img,"png",new File("charindex_after.png"));
 	}
 	
 	
